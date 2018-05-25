e04d61f67b366960e7be6c677d8fadfc73b1a7f2
Jonathan-esquian <36893182+Jonathan-esquian@users.noreply.github.com>
2018-05-25 14:14:27 +0200

    Version 15
    VERSION FINAL DU JEUUUUUU !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Assets/Scripts/MenuCinematique.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCinematique : MonoBehaviour {
+
+	public void PlayMenu (){
+		SceneManager.LoadScene (1);
+	}
+
+}
